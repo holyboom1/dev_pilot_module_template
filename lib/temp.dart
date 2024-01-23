ade059e89af1444510ef12a638dfe57e89c6de4f
Алексей  Лебедев <a.lebedev@enigma.global>
2024-01-23 17:09:43 +0300

    init

---
+++ lib/temp.dart
@@ -0,0 +1,11 @@
+library {module_name};
+
+import 'package:auto_route/auto_route.dart';
+
+import 'temp.gm.dart';
+
+export 'src/screen/{module_name}_screen.dart';
+export '{module_name}.gm.dart';
+
+@AutoRouterConfig.module(replaceInRouteName: 'Screen|Form,Route')
+class TempModule extends $TempModule {}
